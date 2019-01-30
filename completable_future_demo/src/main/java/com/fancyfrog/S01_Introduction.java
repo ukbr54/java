b9795aae8852f3474cf440b0712d58a9e212e2aa
Ujjwal Gupta <ujjwal.gupta@thrymr.net>
2019-01-30 22:00:26 +0530

    Different way of creating Completable Future and running it

--- completable_future_demo/src/main/java/com/fancyfrog/S01_Introduction.java
+++ completable_future_demo/src/main/java/com/fancyfrog/S01_Introduction.java
@@ -3,11 +3,24 @@ package com.fancyfrog;
 import com.fancyfrog.utils.AbstractFuturesTest;
 import org.junit.Test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class S01_Introduction extends AbstractFuturesTest {
 
     @Test
     public void blockingCall(){
         final String title = client.mostRecentQuestionAbout("java");
         System.out.println(String.format("Most recent Java question: %s", title));
+    }
+
+    @Test
+    public void executorService() throws ExecutionException, InterruptedException {
+        final Callable<String> task = () -> client.mostRecentQuestionAbout("java");
+        Future<String> javaQuestionFuture = service.submit(task);
+        //blocking operation
+        final String javaQuestion = javaQuestionFuture.get();
+        System.out.println(String.format("Found : %s", javaQuestion));
     }
 }
---
+++ completable_future_demo/src/main/java/com/fancyfrog/S02_Creating.java
@@ -0,0 +1,41 @@
+package com.fancyfrog;
+
+import com.fancyfrog.utils.AbstractFuturesTest;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/*
+  This class demonstrate the creation of CompletableFuture
+ */
+public class S02_Creating extends AbstractFuturesTest {
+
+    /**
+     * Already completed future
+     */
+    @Test
+    public void completed() throws ExecutionException, InterruptedException {
+        final CompletableFuture<Integer> answer = CompletableFuture.completedFuture(42);
+        final Integer fortyTwo = answer.get();
+    }
+
+    /**
+     * Built-in thread pool
+     */
+    @Test
+    public void supplyAsync() throws ExecutionException, InterruptedException {
+        final CompletableFuture<String> java =
+                CompletableFuture.supplyAsync(() -> client.mostRecentQuestionAbout("java"));
+        System.out.println(String.format("Found: %s",java.get()));
+    }
+
+    /**
+     * Custom thread pool, equivalent (*) to submit()
+     */
+    @Test
+    public void supplyAsyncWithCustomExecutor() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> java = CompletableFuture.supplyAsync(() -> client.mostRecentQuestionAbout("java"), service);
+        System.out.println(String.format("Found: %s", java.get()));
+    }
+}

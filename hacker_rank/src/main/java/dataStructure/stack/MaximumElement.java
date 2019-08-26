e6b26ddbcaac9224d1b3c9ae0de5eee17e068cf1
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2019-08-26 21:14:47 +0530

    Hacker Rank Problems

---
+++ hacker_rank/src/main/java/dataStructure/stack/MaximumElement.java
@@ -0,0 +1,15 @@
+package dataStructure.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MaximumElement {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Stack<Integer> mainStack = new Stack<>();
+        Stack<Integer> maxStack = new Stack<>();
+    }
+}

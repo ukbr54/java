4b181a424d793cb2aacdcf8854e2aee8f11c68ca
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2019-08-26 22:24:00 +0530

    Maximum Element using stack

--- hacker_rank/src/main/java/dataStructure/stack/MaximumElement.java
+++ hacker_rank/src/main/java/dataStructure/stack/MaximumElement.java
@@ -11,5 +11,25 @@ public class MaximumElement {
 
         Stack<Integer> mainStack = new Stack<>();
         Stack<Integer> maxStack = new Stack<>();
+        maxStack.push(Integer.MIN_VALUE);
+
+        for(int i = 0; i < N; i++){
+            int queryType = sc.nextInt();
+            switch (queryType){
+                case 1:
+                    int item = sc.nextInt();
+                    mainStack.push(item);
+                    int maxSoFar = maxStack.peek();
+                    maxStack.push(Math.max(maxSoFar,item));
+                    break;
+                case 2:
+                    mainStack.pop();
+                    maxStack.pop();
+                    break;
+                case 3:
+                    System.out.println(maxStack.peek());
+                    break;
+            }
+        }
     }
 }

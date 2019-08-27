66a32fa9243402eee6beefe1cf16330b3e2aabbe
Ujjwal Gupta <ujjwal.kbr@gmail.com>
2019-08-27 20:07:14 +0530

    Theory'

--- hacker_rank/src/main/java/dataStructure/stack/BalancedBrackets.java
+++ hacker_rank/src/main/java/dataStructure/stack/BalancedBrackets.java
@@ -3,6 +3,20 @@ package dataStructure.stack;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ *  Algorithm :
+ *   1. Create a stack.
+ *   2. while(end of input is not reached)
+ *     a) If the character read is not a symbol to be balanced, ignore it.
+ *     b) If the character is an opening symbol like {,[,( push it into the stack.
+ *     c) If it is a closing symbol like },],) then if the stack is empty report an error. Otherwise pop the stack.
+ *     d) If the symbol is popped is not the corresponding symbol, report an erro.
+ *  3. AT the end of output, if the stack is not empty report an error.
+ *
+ *  Time Complexity: O(n)
+ *  Space Complexity : O(n)
+ */
+
 public class BalancedBrackets {
 
     static String isBalanced(String s) {
